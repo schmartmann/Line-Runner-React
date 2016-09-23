2f6b05e6bd5e95cf726ca4c28414a9bf6e24a299
Stefan Hartmann <stefanhartmann@gmail.com>
2016-09-23 12:43:34 -0400

    add AJAX calls to actions/

---
+++ src/actions/index.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const CREATE_SESSION_URL = 'http://localhost:3001/sessions/create';
+const CREATE_USER_URL = 'http://localhost:3001/users/create';
+const UPLOAD_SCRIPT_URL = 'http://localhost:3001/uploads';
+
+export const NEW_SESSION = 'NEW_SESSION';
+export const NEW_USER = 'NEW_USER';
+export const UPLOAD_SCRIPT = 'UPLOAD_SCRIPT';
+
+export function createSession(){
+  const request = axios.post(`${CREATE_SESSION_URL}`);
+
+  return {
+    type: NEW_SESSION,
+    payload: request
+  };
+}
+
+export function createUser(){
+  const request = axios.post(`${CREATE_USER_URL}`);
+
+  return {
+    type: NEW_USER,
+    payload: request
+  };
+}
+
+export function uploadScript(){
+  const request = axios.post(`${UPLOAD_SCRIPT_URL}`);
+
+  return {
+    type: UPLOAD_SCRIPT,
+    payload: request
+  };
+}
