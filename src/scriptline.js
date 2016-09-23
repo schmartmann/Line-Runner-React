d0230db44ec28126d67133ee0f6571af70f9968d
Stefan Hartmann <stefanhartmann@gmail.com>
2016-09-23 11:57:09 -0400

    successfully connect redux store to props

--- src/scriptline.js
+++ src/scriptline.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class ScriptLine extends Component {
+  renderList(){
+    return this.props.lines.map((line) => {
+      return (
+        <div className="text-container">
+          <p className="script-text" key={line.project}>
+            {line.script_line}
+          </p>
+        </div>
+      )
+    })
+  }
   render(){
     return(
       <div className="ScriptLine">
-        <h3 className="CharacterName">
-          Gaff
-        </h3>
-        <p className="script-text">
-          It's a shame she won't live. But, then again, who does?
-        </p>
+        {this.renderList()}
       </div>
     )
   }
 }
 
-export default ScriptLine;
+function mapStateToProps(state){
+  return {
+    lines: state.lines
+  }
+};
+
+
+export default connect(mapStateToProps)(ScriptLine);
